891c17463b5e872326576f67bd7cbd32e42e46aa
gibbsben <gibbsben98@gmail.com>
2021-02-09 12:14:14 +0100

    test 2

---
+++ build/precache-manifest.0fa492deaad0bb2bae4ffb3a9610a382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d628c7b4452cc454c9ef8ea79b3917",
+    "url": "/gibbsben.github.io/HPM/index.html"
+  },
+  {
+    "revision": "144a25ab04889d729e7c",
+    "url": "/gibbsben.github.io/HPM/static/css/main.4d5c8954.chunk.css"
+  },
+  {
+    "revision": "8ace6249f05c5087dde0",
+    "url": "/gibbsben.github.io/HPM/static/js/2.2f73c1e8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/gibbsben.github.io/HPM/static/js/2.2f73c1e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144a25ab04889d729e7c",
+    "url": "/gibbsben.github.io/HPM/static/js/main.63e3d066.chunk.js"
+  },
+  {
+    "revision": "a28b0d98b2f659db2f44",
+    "url": "/gibbsben.github.io/HPM/static/js/runtime-main.9437d3d8.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gibbsben.github.io/hpm/precache-manifest.0ceef55e971b49d0820fa9d7a20773d6.js"
+  "/gibbsben.github.io/HPM/precache-manifest.0fa492deaad0bb2bae4ffb3a9610a382.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gibbsben.github.io/hpm/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gibbsben.github.io/HPM/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
